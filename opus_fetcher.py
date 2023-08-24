8bbe5b9562c4a024c22f43198bbf62ed3bbd882e
Ignacy Fluder <ignaciofluder@gmail.com>
2023-08-24 14:29:03 +0200

    Gitignore update + minor

--- opus_fetcher.py
+++ opus_fetcher.py
@@ -2,17 +2,22 @@ import undetected_chromedriver as chromedriver
 from undetected_chromedriver import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
+import resources
 import time
 from fp.fp import FreeProxy
 from loguru import logger
 import random
+import os
+import urllib.request
+from selenium.webdriver.common.keys import Keys
 import mail_system, yogopy
 
 ######################################################################
 #timeout should be high for slow proxies such as the free ones i used#
-TIMEOUT = 20#30 #in seconds(rec. for paid: 20, rec. for free: 30)#####
+TIMEOUT = 25#30 #in seconds(rec. for paid: 20, rec. for free: 30)#####
 PROXY_TYPE = "paid"#"free" options are: "paid"(recommended) or "free"#
 PAID_PROXY_BACKBONE = "p.webshare.io:9999"#leave blank for free proxy#
 DO_SLEEP = True#leave true for most uses, changing to false sometimes#
@@ -20,39 +25,29 @@ DO_SLEEP = True#leave true for most uses, changing to false sometimes#
 ######################################################################
 
 
-
 def search_for_element_and_click(browser ,by, selector, _=None):
-    while True:
-        try:
-            element = WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((by, selector)))
-            if DO_SLEEP: time.sleep(2+(random.randint(0,200)/100))
-            element.click()
-            break
-        except TimeoutException:
-            pass
+    element = WebDriverWait(browser, TIMEOUT, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((by, selector)))
+    if DO_SLEEP: time.sleep(2+(random.randint(0,200)/100))
+    element.click()
 
-
-    
 
 def search_for_element_and_send_keys(browser ,by, selector, input_):
-    while True:
-        try:
-            element = WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((by, selector)))
-            if DO_SLEEP: time.sleep(2+(random.randint(0,200)/100))
-            element.click()
-            element.send_keys(input_)
-            break
-        except TimeoutException:
-            pass
+    element = WebDriverWait(browser, TIMEOUT, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((by, selector)))
+    if DO_SLEEP: time.sleep(2+(random.randint(0,200)/100))
+    element.click()
+    element.send_keys(input_)
+    
     
 
 class Fetcher:
     def __init__(self):
-        logger.debug("[OPUS_FETCHER.PY] running with the following settings: TIMEOUT = "+str(TIMEOUT)+", PROXY_TYPE="+str(PROXY_TYPE)+", PAID_PROXY_BACKBONE="+str(PAID_PROXY_BACKBONE)+", DO_SLEEP="+str(DO_SLEEP))
-        count = 1
+        logger.debug("[OPUS_FETCHER.PY] running with the following settings: TIMEOUT="+str(TIMEOUT)+", PROXY_TYPE="+str(PROXY_TYPE)+", PAID_PROXY_BACKBONE="+str(PAID_PROXY_BACKBONE)+", DO_SLEEP="+str(DO_SLEEP))
+        
         self.wish = None
         self.email = "moms-are-hot999@yopmail.com"
 
+    def init(self):
+        count = 1
         while True:
             try:
                 
@@ -69,7 +64,20 @@ class Fetcher:
                 self.driver = chromedriver.Chrome(headless=False,use_subprocess=False, options=self.opt)
                 self.driver.get('https://clip.opus.pro/dashboard?utm_source=opus')
                 
-                frame = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located((By.ID, "app-iframe")))
+                #not sure why this fixes it but it does
+                try:
+                    element = WebDriverWait(self.driver, 5, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Have questions?"]')))
+                    element.click()
+                    time.sleep(5)
+                    element.click()
+                    self.wish = "IP Blocked! "
+                    self.driver.close()
+                    raise Exception("IP Blocked")
+                except TimeoutException:
+                    pass
+                
+
+                frame = WebDriverWait(self.driver, TIMEOUT, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.ID, "app-iframe")))
                 self.driver.switch_to.frame(frame)
                 
                 break
@@ -79,6 +87,7 @@ class Fetcher:
                 time.sleep(1)
 
     def register_step_1(self, email):
+        
         self.email = email
         #insert email
         search_for_element_and_send_keys(self.driver, By.ID, ':r0:', email)
@@ -96,7 +105,7 @@ class Fetcher:
         #Check if ip had been blocked
         time.sleep(3)
         try:
-            element = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Refresh"]')))
+            element = WebDriverWait(self.driver, TIMEOUT, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Refresh"]')))
             element.click()
             self.wish = "IP Blocked! "
             self.driver.close()
@@ -105,7 +114,7 @@ class Fetcher:
             pass
         """
         try:
-            element = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, "//*[text()[contains(.,'Looks like you're connecting through a VPN')]]")))
+            element = WebDriverWait(self.driver, TIMEOUT, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.XPATH, "//*[text()[contains(.,'Looks like you're connecting through a VPN')]]")))
             element.click()
             self.wish = "IP Blocked! "
             self.driver.close()
@@ -121,34 +130,33 @@ class Fetcher:
         search_for_element_and_click(self.driver, By.XPATH, '//button[text()="'+lenght+'"]')
         #Click on submit button
         search_for_element_and_click(self.driver, By.XPATH, '//*[@id="__next"]/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/button')
-        time.sleep(10)
         logger.info('Tracking url: '+self.driver.current_url)
         self.driver.close()
     
     def download_videos(self, url, headless=False):
-        #/html/body/div[1]/div/div/div/div/div[2]/div/div[2]/div[VAR]/div[2]/div/div/div/div/div[2]/div[2]/button
+        #
+        try: self.driver.close()
+        except Exception: pass
+
         self.opt = Options()
-        self.opt.add_argument("--proxy-server="+self.proxy)
+        self.opt.add_argument("--proxy-server=p.webshare.io:9999")
 
         self.driver = chromedriver.Chrome(headless=headless, use_subprocess=False, options=self.opt)
         self.driver.get(url)
 
-        
+        #not sure why this fixes it but it does
         search_for_element_and_click(self.driver, By.XPATH, '//p[text()="Have questions?"]')
         time.sleep(5)
         search_for_element_and_click(self.driver, By.XPATH, '//p[text()="Have questions?"]')
 
-        time.sleep(3)
         search_for_element_and_click(self.driver, By.XPATH, '//button[text()="Download"]')
 
-        frame = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located((By.ID, "app-iframe")))
+        frame = WebDriverWait(self.driver, TIMEOUT, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.ID, "app-iframe")))
         self.driver.switch_to.frame(frame)
 
         self.register_step_1(self.email)
-        time.sleep(1)
         inbox = yogopy.YogoInbox(self.email)
         code = inbox.listen_for_verification_code()
-        time.sleep(2)
 
         #Insert email code
         search_for_element_and_send_keys(self.driver, By.ID, ':r3:', code)
@@ -157,46 +165,48 @@ class Fetcher:
 
         #Check if ip had been blocked
         try:
-            element = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Refresh"]')))
+            element = WebDriverWait(self.driver, TIMEOUT, ignored_exceptions=resources.IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Refresh"]')))
             element.click()
             self.wish = "IP Blocked!"
             self.driver.close()
             raise Exception("IP Blocked")
         except TimeoutException:
             pass
-        
+
         #Scroll down to load dynamic content
-        time.sleep(5)
-        for i in range(1,50):
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
-        
+        page = self.driver.find_element(By.TAG_NAME, 'body')
+        page.click()
+
+        #Get all download links
+        count = 1
+        link_list = []
         while True:
             try:
-                _ = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Download"]')))
-                elements = self.driver.find_elements(By.XPATH, '//button[text()="Download"]')
+                print(str(count))
+                element = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div/div[2]/div['+str(count)+']/div[2]/div/div/div/div/div[2]/div[2]/a')
+                link_list.append(element.get_attribute('href'))
+                page.send_keys(Keys.PAGE_DOWN)
                 if DO_SLEEP: time.sleep(2+(random.randint(0,200)/100))
-                for element in elements:
-                    element.click()
-                    if DO_SLEEP: time.sleep(5+(random.randint(0,200)/100))
+            except NoSuchElementException:
                 break
-            except TimeoutException:
-                pass
+            count += 1
+        
+        dir_ = './temp/'+str(random.randint(0,100000000))
+        os.mkdir(dir_)
 
-        time.sleep(5)
-        for i in range(1,50):
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
-
+        #Download all videos
+        for link in link_list:
+            urllib.request.urlretrieve(link, (dir_+"/"+link.split('/')[-1]).replace('%20', ' '))
+            if DO_SLEEP: time.sleep(2+(random.randint(0,200)/100))
+        
+        logger.info("Path with videos: "+ dir_)
         logger.info("Finished downloading!")
-
-
-        time.sleep(222)
+        self.driver.close()
 
 
         #self.driver.find_elements(By.XPATH, '//button[text()="Download"]')
 
-#fe = Fetcher()
-#while True: fe.download_videos('https://clip.opus.pro/clip/P00816122thP?utm_source=opus')
+fetcher = Fetcher()
+fetcher.download_videos('https://clip.opus.pro/clip/P00816122thP?utm_source=opus')
 
 ##__next > div > div > div > div > div > div.MuiStack-root.css-gojse6 > button.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedSecondary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-root.MuiButton-contained.MuiButton-containedSecondary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.css-1q5zgi7
--- sequence.py
+++ sequence.py
@@ -6,6 +6,7 @@ import time
 import platform
 import yogopy
 
+
 if platform.system() == "Linux":
     os.system("clear")
 else:
@@ -22,17 +23,17 @@ def run(vids:list):
         times -= 1
         try:
             fetcher = opus_fetcher.Fetcher()
-            inbox = yogopy.YogoInbox('forfouchtenredouts1956@yopmail.com')
+            fetcher.init()
+            inbox, email = mail_system.get_inbox()
+            #inbox  = yogopy.YogoInbox('forfouchtenredouts1956@yopmail.com')
 
-            #logger.info('Running program...('+str(times+1)+')')
-            #fetcher.register_step_1(email=email)
+            logger.info('Running program...('+str(times+1)+')')
+            fetcher.register_step_1(email=email)
             
-            #code=inbox.listen_for_verification_code()
-            #fetcher.register_step_2(code=code, yt_link=vids[times])
-            #logger.success("Finished browsing the web!")
-            time.sleep(3)
+            code=inbox.listen_for_verification_code()
+            fetcher.register_step_2(code=code, yt_link=vids[times])
+            logger.success("Finished browsing the web!")
             link = inbox.listen_for_clip_link()
-            time.sleep(3)
             logger.info("Link aquired! "+link)
 
             fetcher.download_videos(url=link)
@@ -40,6 +41,18 @@ def run(vids:list):
             logger.success("Finished registering!")
 
             logger.success("Task NO. "+str(times+1)+" finished!")
+
+        except UnboundLocalError:
+            logger.warning("Unable to identify exception.")
+
+        #make sure the browser closes
+        except KeyboardInterrupt:
+            try: 
+                logger.error("Interrupted")
+                fetcher.driver.close()
+                break
+            except Exception: pass
+
         except Exception as e:
             times +=1
             #ik this looks weird but it works .-.
